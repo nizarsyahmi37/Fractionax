d62b6f54c27cef214081e8da7247f611ba1ff8c5
Nizar Syahmi bin Nazlan <38692430+nizarsyahmi37@users.noreply.github.com>
2025-01-31 17:36:16 +0000

    add: Add temporary coming soon view

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import ViewComingSoon from "@/components/module/view/coming-soon"
+
 export default function Page() {
 	return (
 		<div>
 			<header></header>
-			<main></main>
+			<main
+				className={`min-h-[100vh] items-center content-center`}
+			>
+				<ViewComingSoon />
+			</main>
 			<footer></footer>
 		</div>
 	)
