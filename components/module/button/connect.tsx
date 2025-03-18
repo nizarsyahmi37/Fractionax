3d3d6392d3f9385eba1511c0a82772f79d32b846
Nizar Syahmi bin Nazlan <nizarsyahmi37@gmail.com>
2025-03-19 07:39:53 +0800

    fix: Remove connect button

--- components/module/button/connect.tsx
+++ components/module/button/connect.tsx
@@ -1,24 +1,25 @@
 "use client"
 
-import { truncateAddress } from "@/lib/utils"
-import { useAppKit, useAppKitAccount, useAppKitNetwork, useAppKitState } from "@reown/appkit/react"
-import { Loader } from "lucide-react"
-import { formatEther, getAddress } from "viem"
-import { useBalance } from "wagmi"
+// import { truncateAddress } from "@/lib/utils"
+// import { useAppKit, useAppKitAccount, useAppKitNetwork, useAppKitState } from "@reown/appkit/react"
+// import { Loader } from "lucide-react"
+// import { formatEther, getAddress } from "viem"
+// import { useBalance } from "wagmi"
 
 export default function ButtonConnect() {
-    const { isConnected, address } = useAppKitAccount()
-    const { open } = useAppKit()
-    const state = useAppKitState()
-    const { caipNetwork } = useAppKitNetwork()
+    // const { isConnected, address } = useAppKitAccount()
+    // const { open } = useAppKit()
+    // const state = useAppKitState()
+    // const { caipNetwork } = useAppKitNetwork()
 
-    const result = useBalance({
-        address: getAddress(address || "0x0000000000000000000000000000000000000000")
-    })    
+    // const result = useBalance({
+    //     address: getAddress(address || "0x0000000000000000000000000000000000000000")
+    // })    
 
     return (
-        <button type={`button`} onClick={() => open()} className={`${state.open && "bg-accent text-background"} group border border-accent rounded-xl text-center cursor-pointer font-bold hover:bg-accent hover:border-none hover:text-white`}>
-            {state.loading ? (
+        <button type={`button`}>
+        {/* onClick={() => open()} className={`${state.open && "bg-accent text-background"} group border border-accent rounded-xl text-center cursor-pointer font-bold hover:bg-accent hover:border-none hover:text-white`}> */}
+            {/* {state.loading ? (
                 <div className={`px-4 py-2`}>
                     Loading...
                 </div>
@@ -49,7 +50,7 @@ export default function ButtonConnect() {
                         )
                     )
                 )
-            )}
+            )} */}
         </button>
     )
 }
